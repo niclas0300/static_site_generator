44bcbebf2bcae9cbd8eed5852075885ca2f83e16
niclas0300 <niclasgoehring1@gmail.com>
2025-10-16 22:18:55 +0200

    some new functions :)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode
 
 
 def main():
     text_type = TextType.LINK
     node = TextNode("this is some text", text_type, "https://www.boot.dev")
     print(repr(node))
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.PLAIN:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {'href':text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"invalid text type: {text_node.text_type}")
 
 main()
---
+++ src/test_html_func.py
@@ -0,0 +1,29 @@
+from main import text_node_to_html_node
+
+import unittest
+from textnode import TextNode, TextType
+from htmlnode import LeafNode, ParentNode
+
+
+class TestHTMLFunc(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.to_html(), '<a href="boot.dev">This is a text node</a>')
+
+    def test_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.to_html(), "<i>This is a text node</i>")
+
+
+if __name__ == "__main__":
+    unittest.main()
