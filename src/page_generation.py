9e80f03b459bf9591c4ddd0ffdbdcf1bb8dcc4ea
niclas0300 <niclasgoehring1@gmail.com>
2025-10-30 20:07:49 +0100

    done!

--- src/page_generation.py
+++ src/page_generation.py
@@ -3,13 +3,15 @@ from htmlnode import ParentNode
 from extract_title import extract_title
 import os
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_path_md_file = open(from_path).read()
     template_file = open(template_path).read()
     html_string = markdown_to_html_node(from_path_md_file).to_html()
     html_title = extract_title(from_path_md_file)
     new_template = template_file.replace("{{ Title }}", html_title).replace("{{ Content }}", html_string)
+    content_html_file = content_html_file.replace('href="/', f'href="{basepath}')
+    content_html_file = content_html_file.replace('src="/', f'src="{basepath}')    
     if not os.path.exists(dest_path):
         os.mkdir(dest_path)
     dest_file_path = os.path.join(dest_path, "index.html")
@@ -40,11 +42,11 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
             content_html_file = content_html_file.replace('src="/', f'src="{basepath}')
 
             if not os.path.exists(dest_dir_path):
-                os.mkdir(dest_dir_path)
+                os.makedirs(dest_dir_path)
 
             dest_file_path = os.path.join(dest_dir_path, "index.html")
             to_file = open(dest_file_path, "w")
             to_file.write(content_html_file)
             to_file.close()
         else:
-            generate_pages_recursive(joined_dir_path, template_path, joined_dest_path)
+            generate_pages_recursive(joined_dir_path, template_path, joined_dest_path, basepath)
